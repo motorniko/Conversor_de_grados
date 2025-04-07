24e255098341b7e988a46e3adbfb5246e1c1712c
motorniko <albertolagos59@gmail.com>
2025-04-06 22:25:36 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        double gradosCelsius = 25.0;
+        double gradosFahrenheit = (gradosCelsius * 1.8) + 32;
+
+        System.out.println("La temperatura en grados fahrenheit es: " + gradosFahrenheit);
+
+    }
+}
